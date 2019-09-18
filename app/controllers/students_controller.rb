ea5233e02061a2b793e1b5a4803ba8beaafcf832
Nicole Dow <nikki.dow71@gmail.com>
2019-09-18 15:16:56 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,17 @@
 class StudentsController < ApplicationController
+
+    def new
+    end
+
+    def create
+      @student = Student.create(student_params)
+      redirect_to "/students/#{@student.id}"
+    end
+
+
+
+    private
+    def student_params
+      params.require(:student).permit(:first_name, :last_name)
+    end
 end
